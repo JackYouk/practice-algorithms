782ee4ee0b3b9f6e9c1042240d9f8eda2c24669e
Jack Youkstetter <jackyoukstetter1@gmail.com>
2022-06-17 17:34:23 -0700

    added mannys solution- switch function

--- dayOfTheWeek.js
+++ dayOfTheWeek.js
@@ -8,3 +8,27 @@ function findDay(num){
 console.log(findDay(1));
 console.log(findDay(5));
 console.log(findDay(8));
+
+// manny's solution (switch statement)
+const getDayOfWeek = (number) => {
+    switch(number) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return undefined;
+    }
+  };
+  
+  console.log(getDayOfWeek(6));
